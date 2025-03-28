583dedbdf4e551230ec7a0ecc00e433c33760df0
squirel49 <jackmorganfaulkner@gmail.com>
2025-03-28 03:13:30 +0000

    moved to base dir

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import legacy from '@vitejs/plugin-legacy'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    legacy()
+  ],
+  server: {
+    allowedHosts: ['pathslesstravelled.xyz'],
+    host: true,
+    port: 80
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  }
+})
